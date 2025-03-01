23f9895d9a01c35407d0882d1f6aa15a1d2cccfe
Jerome <f4deb1@gmail.com>
2025-03-01 10:33:50 +0000

    DEV CONTENAIR

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/f4deb/esp/v5.4/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/f4deb/esp/v5.4/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
